8cee66ca90a620ec0e30b4c97aa6b2202134f02a
Amanda Ciobanu <amanda@ciobanu.org>
2020-12-15 15:31:20 +0000

    feat: create pages folder. Creat page files for login, register, error, forgot password and home. Updated index.js and index.html files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import pages
+import Register from "./pages/Register";
+import Login from './pages/Login';
+import Error from './pages/Error';
+import Home from './pages/Home';
+
+
+// import components
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
   );
 }
 
