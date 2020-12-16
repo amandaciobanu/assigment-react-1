c1ee5b3a56f4dc5d7f969e9900697f73edff8d2c
Amanda Ciobanu <amanda@ciobanu.org>
2020-12-16 09:34:57 +0000

    feat: mock login user service. Global service not yet defined

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,81 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { login } from '../services/Auth'
+import { Link, Redirect } from 'react-router-dom'
 
 export default function LoginForm() {
+
+  const [authUser, setAuthUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [requesting, setRequesting] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+
+  const performLogIn = async (e) => {
+    e.preventDefault();
+
+    setRequesting(true);
+    const result = await login(username, password);
+    if (result) {
+      setLoginError(false);
+      setAuthUser(result);
+    }
+    else {
+      setLoginError(true);
+    }
+
+    setRequesting(false);
+  }
+
+  if (authUser) {
+    return <Redirect to='/' />;
+  }
+
   return (
       <form>
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          <input type="email"
+                 className="form-control"
+                 id="email"
+                 aria-describedby="emailHelp"
+                 onChange={e => {
+                   setUsername(e.target.value);
+                 }}
+          />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
               else.</small>
         </div>
+
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1"/>
+          <input
+              type="password"
+              className="form-control"
+              id="password"
+              onChange={e => {
+                setPassword(e.target.value);
+              }}
+          />
         </div>
-        <div className="form-group form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+
+        {loginError && <p style= color:"red">Login failed</p>}
+
+        <button
+            id='fetch-button'
+            type="submit"
+            disabled={username.length === 0 || password.length === 0 || requesting}
+            className="btn btn-primary" onClick={performLogIn}
+        >
+          {requesting ? 'Logging in...' : 'Log in'}
+        </button>
+        <div>
+          <Link to="/forgotPassword">Forgot your password?</Link>
+
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <div>
+          <Link to="/register">Don't have an account?</Link>
+
+        </div>
       </form>
   )
 }
---
+++ src/services/Auth.js
@@ -0,0 +1,22 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function login(username, password) {
+
+  await sleep(2000);
+
+  if (username === "amanda@ciobanu.org") {
+    return {
+      firstName: "Amanda",
+      lastName: "Ciobanu",
+      email: "amanda@ciobanu.org"
+    };
+  }
+
+  return null;
+}
+
+export  {
+  login
+}
