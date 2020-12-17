bdd8cb612cf080fea6637850ef0d970e4ed11b8d
Amanda Ciobanu <amanda@ciobanu.org>
2020-12-17 17:09:37 +0000

    style: added new banner and text for homepage/ add new favicon to page/cleaned up index.html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,16 @@ import React from "react";
 
 export default function Home(){
   return(
-      <div></div>
+        <div className="container-fluid container-homepage">
+          <div className="row">
+            <div className='col-6 align-self-center'>
+              <div className='banner-content'>
+                <p>UNLEASH YOUR INNER CHAMPION</p>
+                <h1>Basketball and technology at its best</h1>
+                <p>FX will help you to track your progress and freeing up time for what really matters.</p>
+              </div>
+            </div>
+          </div>
+        </div>
   )
 }
