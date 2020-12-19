642c6ad820fafa228eb4e45556b6bd83fc1bae6d
Amanda Ciobanu <amanda@ciobanu.org>
2020-12-19 14:13:05 +0000

    style: Added style to error page. Fix few style issues

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function Error(){
   return(
-      <div></div>
+      <div className="container-fluid login-form">
+        <div className="row">
+          <div className='col-12 error-page'>
+            <h3>Sorry :(</h3>
+            <h3>Something went wrong.</h3>
+          </div>
+        </div>
+     </div >
   )
 }
